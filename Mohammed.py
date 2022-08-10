554d77b30d12383498c9b23a8758871fa331d976
Mohammed-kashif <kashifmd134@gmail.com>
2022-08-10 17:01:13 +0530

    2 files added

---
+++ Mohammed.py
@@ -0,0 +1,12 @@
+ Volume in drive C has no label.
+ Volume Serial Number is 9C33-08A4
+
+ Directory of C:\Users\mohammedkashif\Desktop\python
+
+10/08/2022  16:59    <DIR>          .
+10/08/2022  16:59    <DIR>          ..
+10/08/2022  15:49                80 Firstproject.py
+10/08/2022  16:59               406 index.txt
+10/08/2022  16:59                 0 Mohammed.py
+               3 File(s)            486 bytes
+               2 Dir(s)  14,038,978,560 bytes free
